15f5e184ebbe410d339e072a74f3fe6afcd20bdd
givxq <coooeeeeep@gmail.com>
2022-12-17 09:49:38 +0900

    [basic5]Cond,With 추가

--- basic_5_control_structures.ex
+++ basic_5_control_structures.ex
@@ -56,3 +56,45 @@ case {1, 2, 3} do
   _ ->
     "Won't match"
 end
+
+
+# Cond
+cond do
+  2 + 2 == 5 ->
+    "This will not be true"
+  2 * 2 == 3 ->
+    "Nor this"
+  1 + 1 == 3 ->
+    "But this will"
+  true -> "Catch all"
+end
+
+# With
+user = %{first: "Sean", last: "Callan"}
+with {:ok, first} <- Map.fetch(user, :first),
+    {:ok, last} <- Map.fetch(user, :last),
+    do: last <> ", " <> first
+
+user = %{first: "doomspork"}
+with {:ok, first} <- Map.fetch(user, :first),
+    {:ok, last} <- Map.fetch(user, :last),
+    do: last <> ", " <> first
+
+case Repo.insert(changeset) do
+  {:ok, user} ->
+    case Guardian.encode_and_sign(user, :token, claims) do
+      {:ok, token, full_claims} ->
+        important_stuff(token, full_claims)
+
+      error ->
+        error
+    end
+
+  error ->
+    error
+end
+
+with  {:ok, user} <- Repo.insert(changeset),
+      {:ok, token, full_claims} <- Guardian.encode_and_sign(user, :token, claims) do
+        important_stuff(token, full_claims)
+      end
