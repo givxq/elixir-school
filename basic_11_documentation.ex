7b75262fcb8faad1f01274a68ecf3c4d95798361
givxq <coooeeeeep@gmail.com>
2022-12-19 22:10:21 +0900

    [basic 11] doc 정리

--- basic_11_documentation.ex
+++ basic_11_documentation.ex
@@ -23,3 +23,22 @@ end
 
 # 함수문서화하기
 # Elixir는 모듈 범위에서 주석을 달 수 있게 해주었듯이, 함수 범위에서도 비슷한 방식으로 주석을 달 수 있게 해줍니다. @doc 주석자는 함수 수준에서의 인라인 주석을 가능하게 해줍니다. @doc 주석자는 주석을 달고자 하는 함수 선언부의 바로 위쪽에 위치합니다.
+
+
+# 모듈은 문서화하고자 하는 것이 아니라면, @moduledoc 을 공백으로 비워두지 마세요. 차라리 다음과 같이 false 로 두는 게 낫습니다.
+defmodule Greeter do
+  @moduledoc false
+end
+
+
+# 모듈 문서 내에서 함수를 설명하고자 할 때, 다음과 같이 역 따옴표를 이용해보세요.
+defmodule Greeter do
+  @moduledoc """
+  ...
+  이 모듈도 `hello/1` 함수를 가지고 있습니다.
+  """
+
+  def hello(name) do
+    IO.puts("Hello, " <> name)
+  end
+end
